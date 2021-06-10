39b10d967a9f51b1af4ec3a8516d641071180f5a
Amine Hilaly <hilalyamine@gmail.com>
2021-06-10 12:24:51 +0200

    Add v1

---
+++ apis/v1/doc.go
@@ -0,0 +1,4 @@
+// +k8s:deepcopy-gen=package
+// Package v1 is the v1 version of the ecr.services.k8s.aws API.
+// +groupName=ecr.services.k8s.aws
+package v1
--- pkg/resource/repository/sdk.go
+++ pkg/resource/repository/sdk.go
@@ -27,7 +27,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	svcapitypes "github.com/aws-controllers-k8s/ecr-controller/apis/v1alpha1"
+	svcapitypes "github.com/aws-controllers-k8s/ecr-controller/apis/v1"
 )
 
 // Hack to avoid import errors during build...
