ea403ae67fa20a667adf93268d7329b4f157ce02
Amine Hilaly <hilalyamine@gmail.com>
2021-07-01 13:26:38 +0200

    add

---
+++ apis/v2/v3/setup.go
@@ -0,0 +1,18 @@
+package v2
+
+import (
+	"fmt"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var logg = logf.Log.WithName("repository-resource")
+
+func (r *Repository) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	fmt.Println("setting up webhook v2")
+
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
